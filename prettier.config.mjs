52d99efb3d4a8aec096738cba13fed58ce81441d
raintje <wesselvandorsten@proton.me>
2025-10-02 19:48:38 +0200

    🎨 [chore]: Prettier/ESLint configuration
    Added Prettier configuration and ran the linter.

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type { import('prettier').Config } */
+export default {
+    semi: true,
+    trailingComma: 'es5',
+    printWidth: 140,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    endOfLine: 'crlf',
+    tabWidth: 4,
+};
